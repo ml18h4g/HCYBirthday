0f7bada0c6957b2fa27e3126ba3b3964f72786ba
Hz G <hzg@192.168.0.12>
2021-01-15 22:48:45 +0800

    ghz commit

--- app/src/main/java/com/hewhywhy/hcybirthday/MainActivity.java
+++ app/src/main/java/com/hewhywhy/hcybirthday/MainActivity.java
@@ -60,13 +60,17 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.view.Gravity;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView msgListView;
-    private EditText inputText;
-    private Button send;
+    private Button inputText;
+//    private Button send;
     private MsgAdapter adapter;
+    private Button button;
 
     private List<Msg> msgList = new ArrayList<Msg>();
 
@@ -74,37 +78,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActionBar actionBar = getSupportActionBar();
+//        ActionBar actionBar=getSupportActionBar();
+//        actionBar.setCustomView(R.layout.actionbar_title);
+//        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//        ActionBar actionBar = getSupportActionBar();
 
         setContentView(R.layout.activity_main);
 
-        initMsgs();
+        ActionBar actionBar = getSupportActionBar();
+            if(actionBar != null){
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+                actionBar.setCustomView(R.layout.actionbar_title);
+                TextView textView = (TextView) actionBar.getCustomView().findViewById(R.id.display_title);
+                textView.setText("华晨宇");
+                actionBar.setHomeButtonEnabled(true);
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowCustomEnabled(true);
+            }
+
+
+       initMsgs();
+//       continueMsgs();
         adapter = new MsgAdapter(MainActivity.this, R.layout.msg_item, msgList);
-        inputText = (EditText)findViewById(R.id.input_text);
-        send = (Button)findViewById(R.id.send);
-        msgListView = (ListView)findViewById(R.id.msg_list_view);
+//        inputText = (EditText) findViewById(R.id.input_text);
+        button = (Button) findViewById(R.id.button_id);
+//        send = (Button) findViewById(R.id.send);
+        msgListView = (ListView) findViewById(R.id.msg_list_view);
         msgListView.setAdapter(adapter);
-        send.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String content = inputText.getText().toString();
-                if(!"".equals(content)) {
+                if (!"".equals(content)) {
                     Msg msg = new Msg(content, Msg.TYPE_SEND);
                     msgList.add(msg);
                     adapter.notifyDataSetChanged();
                     msgListView.setSelection(msgList.size());
                     inputText.setText("");
+
                 }
             }
         });
     }
 
-    private void initMsgs() {
-        Msg msg1 = new Msg("How are you?", Msg.TYPE_RECEIVED);
-        msgList.add(msg1);
-        Msg msg2 = new Msg("Fine, thank you, and you?", Msg.TYPE_SEND);
-        msgList.add(msg2);
-        Msg msg3 = new Msg("I am fine, too!", Msg.TYPE_RECEIVED);
-        msgList.add(msg3);
+        private void initMsgs() {
+            Msg msg1 = new Msg("Hi~杨善惠，好久不见啦！", Msg.TYPE_RECEIVED);
+            msgList.add(msg1);
+            Msg msg2 = new Msg("是啊！见到花花好开心！花花在干嘛？", Msg.TYPE_SEND);
+            msgList.add(msg2);
+            Msg msg3 = new Msg("我在练歌哦，想听我唱歌么？给你个机会点歌我唱给你听！", Msg.TYPE_RECEIVED);
+            msgList.add(msg3);
     }
+//}
+
+//    private void continueMsgs() {
+//        switch(button_id){
+//            case 1:
+//                Msg msg4 = new Msg("太好了！我想听烟火里的尘埃！", Msg.TYPE_SEND);
+//                msgList.add(msg4);
+//                break;
+//            case 2:
+//                Msg msg5 = new Msg("太好了！我想听国王与乞丐！", Msg.TYPE_SEND);
+//                msgList.add(msg5);
+//                break;
+//
+//
+//
+//
+//        }
+//
+//    }
 }
+
